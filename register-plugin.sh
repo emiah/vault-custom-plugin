73754b1d4be314bb3f8eb2ac3c259de1fd272707
longdv <longhn92@gmail.com>
2025-04-01 10:02:27 +0700

    Create register-plugin.sh

---
+++ register-plugin.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+# Calculate SHA256 checksum of the plugin
+PLUGIN_SHASUM=$(sha256sum /vault/plugins/vault-plugin-database-oracle | cut -d ' ' -f1)
+
+# Register the plugin with Vault using its SHA256 checksum
+vault plugin register -sha256="$PLUGIN_SHASUM" database vault-plugin-database-oracle
+
+# Add the plugin to Vault's plugin catalog
+vault write sys/plugins/catalog/database/vault-plugin-database-oracle \
+    sha256="$PLUGIN_SHASUM" \
+    command="vault-plugin-database-oracle"
